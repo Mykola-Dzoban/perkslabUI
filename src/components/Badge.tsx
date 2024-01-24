414cee2664cdc9b0559b1c28cfcacdffdaa481e9
Mykola Dzoban <nick.dzoban@gmail.com>
2024-01-24 19:29:49 +0200

    changed to typescript and started working on badge component

---
+++ src/components/Badge.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+interface CardProps {}
+
+const Badge: React.FC<CardProps> = () => {
+	return <div className="bg-gray-100 p-4 rounded-md shadow-md w-12">Badge</div>;
+};
+
+export default Badge;
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import Card from './components/Card';
-import Button from './components/Button';
-
-export { Card, Button };
