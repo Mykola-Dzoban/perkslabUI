e721e27e6b1117095322cfdc3174f63e69888c32
Mykola Dzoban <nick.dzoban@gmail.com>
2024-01-24 21:06:02 +0200

    badge: added 5 types and styles for each type

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import '../styles.css';
 
-interface CardProps {}
+interface CardProps {
+	type: string;
+	className?: string;
+	children?: ReactNode;
+	onClick?: React.MouseEventHandler<HTMLDivElement>;
+}
 
-const Badge: React.FC<CardProps> = () => {
-	return <div className="bg-gray-100 p-4 rounded-md shadow-md w-12">Badge</div>;
+const Badge: React.FC<CardProps> = ({ className, children, onClick, type }) => {
+	const types: { [key: string]: string } = {
+		none: 'bg-white border-2 px-2 w-fit border-zinc-950',
+		standard: 'bg-white border-2 w-fit px-2 border-zinc-950 hover:font-medium hover:bg-gray-200',
+		primary: 'bg-green-200 border-2 w-fit px-2 border-zinc-950 hover:font-medium hover:bg-green-300',
+		neutral: 'bg-neutral-600 text-gray-50 border-2 w-fit px-2 border-zinc-950 hover:font-medium hover:bg-neutral-700',
+		destructive: 'bg-red-400 border-2 w-fit px-2 border-zinc-950 hover:font-medium hover:bg-red-500',
+	};
+
+	const badgeClasses = `${types[type]} ${className ?? ''}`;
+
+	return (
+		<div className={badgeClasses} onClick={onClick}>
+			{children || 'badge'}
+		</div>
+	);
 };
-
 export default Badge;
