3c497469b5c61bd77b6c0d97aafa441516ddd29e
Mykola Dzoban <nick.dzoban@gmail.com>
2024-07-14 09:37:32 +0300

    added border radius to card, added checking nesting of elements

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { cn } from '../utils';
-import './styles.css';
+
 
 interface CardProps extends React.HTMLAttributes<HTMLDivElement> {}
 interface CardHeaderProps extends React.HTMLAttributes<HTMLDivElement> {}
-interface CardTitleProps extends React.HTMLAttributes<HTMLSpanElement> {}
+interface CardTitleProps extends React.HTMLAttributes<HTMLHeadingElement> {}
 interface CardDescriptionProps extends React.HTMLAttributes<HTMLSpanElement> {}
 interface CardContentProps extends React.HTMLAttributes<HTMLDivElement> {}
 interface CardFooterProps extends React.HTMLAttributes<HTMLDivElement> {}
@@ -14,31 +14,64 @@ interface CardImageProps extends React.HTMLAttributes<HTMLImageElement> {
 	alt: string;
 }
 
+interface CardContextProps {
+	isInsideCard: boolean;
+}
+interface CardHeaderContextProps {
+	isInsideCardHeader: boolean;
+}
+interface CardImageContainerContextProps {
+	isInsideCardImageContainer: boolean;
+}
+
+const CardContext = React.createContext<CardContextProps | undefined>(undefined);
+const CardHeaderContext = React.createContext<CardHeaderContextProps | undefined>(undefined);
+const CardImageContainerContext = React.createContext<CardImageContainerContextProps | undefined>(undefined);
+
 export const Card: React.FC<CardProps> = ({ className, children, ...props }) => {
 	return (
-		<div className={cn('bg-cyan-100 border-2 border-zinc-950 p-4 flex flex-col gap-3', className)} {...props}>
-			{children}
-		</div>
+		<CardContext.Provider value={{ isInsideCard: true }}>
+			<div className={cn('bg-cyan-100 border-2 border-zinc-950 p-4 flex flex-col gap-3 rounded-lg w-80', className)} {...props}>
+				{children}
+			</div>
+		</CardContext.Provider>
 	);
 };
 
 export const CardHeader: React.FC<CardHeaderProps> = ({ className, children, ...props }) => {
+	const cardContext = React.useContext(CardContext);
+
+	if (!cardContext?.isInsideCard) {
+		throw new Error('Card Header can only be used inside Card');
+	}
 	return (
-		<div className={cn('flex flex-col gap-3', className)} {...props}>
-			{children}
-		</div>
+		<CardHeaderContext.Provider value={{ isInsideCardHeader: true }}>
+			<div className={cn('flex flex-col gap-3', className)} {...props}>
+				{children}
+			</div>
+		</CardHeaderContext.Provider>
 	);
 };
 
 export const CardTitle: React.FC<CardTitleProps> = ({ className, children, ...props }) => {
+	const cardHeaderContext = React.useContext(CardHeaderContext);
+
+	if (!cardHeaderContext?.isInsideCardHeader) {
+		throw new Error('Card Title can only be used inside Card Header');
+	}
 	return (
-		<span className={cn('font-bold text-2xl leading-none', className)} {...props}>
+		<h3 className={cn('font-bold text-2xl leading-none', className)} {...props}>
 			{children}
-		</span>
+		</h3>
 	);
 };
 
 export const CardDescription: React.FC<CardDescriptionProps> = ({ className, children, ...props }) => {
+	const cardHeaderContext = React.useContext(CardHeaderContext);
+
+	if (!cardHeaderContext?.isInsideCardHeader) {
+		throw new Error('Card Description can only be used inside Card Header');
+	}
 	return (
 		<span className={cn(' text-base leading-none', className)} {...props}>
 			{children}
@@ -47,6 +80,11 @@ export const CardDescription: React.FC<CardDescriptionProps> = ({ className, chi
 };
 
 export const CardContent: React.FC<CardContentProps> = ({ className, children, ...props }) => {
+	const cardContext = React.useContext(CardContext);
+
+	if (!cardContext?.isInsideCard) {
+		throw new Error('Card Content can only be used inside Card');
+	}
 	return (
 		<div className={cn('flex', className)} {...props}>
 			{children}
@@ -55,6 +93,11 @@ export const CardContent: React.FC<CardContentProps> = ({ className, children, .
 };
 
 export const CardFooter: React.FC<CardFooterProps> = ({ className, children, ...props }) => {
+	const cardContext = React.useContext(CardContext);
+
+	if (!cardContext?.isInsideCard) {
+		throw new Error('Card Footer can only be used inside Card');
+	}
 	return (
 		<div className={cn('flex gap-3 items-center', className)} {...props}>
 			{children}
@@ -63,14 +106,26 @@ export const CardFooter: React.FC<CardFooterProps> = ({ className, children, ...
 };
 
 export const CardImageContainer: React.FC<CardImageContainerProps> = ({ className, children, ...props }) => {
+	const cardContext = React.useContext(CardContext);
+
+	if (!cardContext?.isInsideCard) {
+		throw new Error('Card Image Container can only be used inside Card');
+	}
 	return (
-		<div className={cn('w-full h-full', className)} {...props}>
-			{children}
-		</div>
+		<CardImageContainerContext.Provider value={{ isInsideCardImageContainer: true }}>
+			<div className={cn('w-full h-full', className)} {...props}>
+				{children}
+			</div>
+		</CardImageContainerContext.Provider>
 	);
 };
 
 export const CardImage: React.FC<CardImageProps> = ({ className, children, src, alt, ...props }) => {
+	const cardImageContainerContext = React.useContext(CardImageContainerContext);
+
+	if (!cardImageContainerContext?.isInsideCardImageContainer) {
+		throw new Error('Card Image can only be used inside Card Image Container');
+	}
 	return (
 		<img src={src} alt={alt} className={cn('w-full h-full', className)} {...props}>
 			{children}
